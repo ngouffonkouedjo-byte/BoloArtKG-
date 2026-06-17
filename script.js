fcedabb18ce103656ca4e25f1a110e342fb49b44
ngouffonkouedjo-byte <ngouffonkouedjo@gmail.com>
2026-06-17 13:15:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+body {
+  font-family: Arial;
+  text-align: center;
+  background: #f4f4f4;
+}
 
+h1 {
+  color: darkblue;
+}
+
+div {
+  background: white;
+  margin: 20px;
+  padding: 15px;
+  display: inline-block;
+  border-radius: 10px;
+}
